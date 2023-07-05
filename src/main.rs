d80814c2eefe669e3586b1ec36fce7cb38e66c16
Joel B <smol@toast.cyou>
2023-07-05 15:09:16 -0400

    added threading controls

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use gtk::{Application, ApplicationWindow, HeaderBar, Box, Entry, ScrolledWindow,
 use gdk::{keys::constants as key, EventKey};
 extern crate glib;
 use gtk::prelude::*;
-use glib::Sender;
+use glib::{num_processors, Sender};
 use std::thread;
 use tokio::runtime::Runtime;
 use tokio::runtime::Handle;
@@ -27,13 +27,14 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 fn build_ui(application: &Application) {
+    // Define window attributes
     let window = ApplicationWindow::new(application);
     window.set_title("Chat with AI");
     window.set_default_size(700, 400);
-
+    // prep headerbar
     let header = HeaderBar::new();
     header.set_title(Some("AI Models"));
-
+    // create combobox for headerbar
     let model_combo = ComboBoxText::new();
     model_combo.set_hexpand(false);
     let bin_files = enumerate_bin_files();
@@ -42,33 +43,33 @@ fn build_ui(application: &Application) {
         model_combo.append_text(&file_name);
     }
     model_combo.set_active(Some(0));
-
     header.pack_start(&model_combo);
     header.set_hexpand(false);
-
+    // initialize main box
     let main_box = Box::new(Orientation::Vertical, 5);
     main_box.set_margin_top(0);
     main_box.set_margin_bottom(0);
     main_box.set_margin_start(0);
     main_box.set_margin_end(0);
-
+    // initialize scrolling container
     let scrolled_window = ScrolledWindow::new(None::<&Adjustment>, None::<&Adjustment>);
     scrolled_window.set_hexpand(true);
     scrolled_window.set_vexpand(true);
-
+    // create textview and buffer for scrolling window
     let text_buffer = TextBuffer::new(Some(&TextTagTable::new()));
     let text_view = TextView::new();
     text_view.set_wrap_mode(gtk::WrapMode::Word);
     text_view.set_editable(true);
     text_view.set_border_width(10);
-
+    text_view.set_pixels_below_lines(5);
+    // initialize text buffer with neccesary attributes
     let mut buffer = TextBuffer::new(None::<&gtk::TextTagTable>);
     text_view.set_buffer(Some(&buffer));
     buffer.set_text("How can I help you today?");
-
+    // glue it all together
     scrolled_window.add(&text_view);
     main_box.pack_start(&scrolled_window, true, true, 0);
-
+    // create entry box
     let entry_buffer = EntryBuffer::new(None);
     let entry = Entry::with_buffer(&entry_buffer);
     let runtime = Rc::new(Runtime::new().unwrap());
@@ -76,33 +77,32 @@ fn build_ui(application: &Application) {
     entry.set_margin_top(5);
     entry.set_margin_start(5);
     entry.set_margin_end(5);
-
+    // put entry box at bottom of app window
     main_box.pack_start(&entry, false, false, 0);
-
+    // initialize send button for headerbar
     let send_button = Button::with_label("Send");
     send_button.set_relief(ReliefStyle::None);
-
     send_button.connect_clicked(glib::clone!(@weak entry => move |_| {
         let temp_entry = entry.clone();
         temp_entry.activate(); // Simulate Enter key press
         temp_entry.grab_focus();
     }));
-
     header.pack_end(&send_button);
-
+    // initialize settings button for headerbar
     let settings_button = Button::with_label("Settings");
     settings_button.set_relief(ReliefStyle::None);
     header.pack_end(&settings_button);
-
+    // display popover window when clickking settings
     let popover = Popover::new(Some(&settings_button));
     popover.set_position(gtk::PositionType::Bottom);
-
+    // oh boy get ready
     let settings_list = ListBox::new();
     settings_list.set_margin_top(10);
     settings_list.set_margin_bottom(10);
     settings_list.set_margin_start(10);
     settings_list.set_margin_end(10);
-
+    settings_list.set_border_width(5);
+    // WARNING !!! SHIT IS FUCKED PAST THIS POINT !!! YOU HAVE BEEN WARNED
     // Initial Prompt Entry
     let prompt_label = Label::new(Some("Initial prompt entry:"));
     let prompt_buffer = EntryBuffer::new(None);
@@ -112,7 +112,6 @@ fn build_ui(application: &Application) {
     prompt_row.pack_start(&prompt_label, false, false, 0);
     prompt_row.pack_start(&prompt_entry, true, true, 0);
     settings_list.add(&prompt_row);
-
     // Maximum Generation Length
     let length_label = Label::new(Some("Maximum generation length:"));
     let length_adjustment = Adjustment::new(256.0, 0.0, 2048.0, 1.0, 1.0, 0.0);
@@ -122,7 +121,6 @@ fn build_ui(application: &Application) {
     length_row.pack_start(&length_label, false, false, 0);
     length_row.pack_start(&length_spin_button, false, false, 0);
     settings_list.add(&length_row);
-
     // Toggles for Sampling and Early Stopping
     let sampling_switch = Switch::new();
     let sampling_label = Label::new(Some("Sampling"));
@@ -130,14 +128,13 @@ fn build_ui(application: &Application) {
     sampling_row.pack_start(&sampling_switch, false, false, 0);
     sampling_row.pack_start(&sampling_label, false, false, 0);
     settings_list.add(&sampling_row);
-
+    // Stopping Switch
     let stopping_switch = Switch::new();
     let stopping_label = Label::new(Some("Early Stopping"));
     let stopping_row = gtk::Box::new(Orientation::Horizontal, 5);
     stopping_row.pack_start(&stopping_switch, false, false, 0);
     stopping_row.pack_start(&stopping_label, false, false, 0);
     settings_list.add(&stopping_row);
-
     // Temperature Float
     let temperature_label = Label::new(Some("Temperature float:"));
     let temperature_adjustment = Adjustment::new(0.7, 0.0, 100.0, 0.1, 1.0, 0.0);
@@ -147,7 +144,6 @@ fn build_ui(application: &Application) {
     temperature_row.pack_start(&temperature_label, false, false, 0);
     temperature_row.pack_start(&temperature_spin_button, false, false, 0);
     settings_list.add(&temperature_row);
-
     // Beam Number (1-16)
     let beam_label = Label::new(Some("Beam count (1-16):"));
     let beam_adjustment = Adjustment::new(4.0, 1.0, 16.0, 1.0, 1.0, 0.0);
@@ -157,9 +153,19 @@ fn build_ui(application: &Application) {
     beam_row.pack_start(&beam_label, false, false, 0);
     beam_row.pack_start(&beam_spin_button, false, false, 0);
     settings_list.add(&beam_row);
-
+    // Thread count
+    let thread_limit = num_processors();
+    let thread_label = Label::new(Some(&format!("Thread limit (1-{})", thread_limit)));
+    let thread_adjustment = Adjustment::new(2.0, 1.0, thread_limit as f64, 1.0, 1.0, 0.0);
+    let thread_spin_button = SpinButton::new(Some(&thread_adjustment), 1.0, 0);
+    thread_spin_button.set_numeric(true);
+    let thread_row = gtk::Box::new(Orientation::Horizontal, 5);
+    thread_row.pack_start(&thread_label, false, false, 0);
+    thread_row.pack_start(&thread_spin_button, false, false, 0);
+    settings_list.add(&thread_row);
+    // add that shit to the popover
     popover.add(&settings_list);
-
+    // tie it together
     settings_button.connect_clicked(move |_| {
         popover.show_all();
     });
@@ -178,6 +184,7 @@ fn build_ui(application: &Application) {
         let temp = temperature_adjustment.value() as f32;
         let beams = beam_adjustment.value() as u8;
         let model_name = std::string::String::from(model_combo.active_text().unwrap());
+        let num_cpus = num_processors();
         println!("habbening =D {}", &text);
         // Get an iterator pointing to the end of the buffer
         let mut end_iter = buffer.end_iter();
@@ -186,6 +193,7 @@ fn build_ui(application: &Application) {
         buffer.insert(&mut end_iter_mut, &text);
         // insert ebic threading code here ( you know ;) )
         thread::spawn(move || {
+            let entrytext = text.clone();
             // This closure will be executed in the new thread
             glib::idle_add(move || {
                 // This closure will be executed in the main GTK thread
@@ -194,10 +202,10 @@ fn build_ui(application: &Application) {
                 let params: gpt_params_c = {
                     gpt_params_c {
                         n_threads: 8,
-                        temp: 0.0,
+                        temp: temp,
                         use_mlock: true,
                         model: str_to_mut_i8(&format!("/home/toast/.ai/{}", model_name)),
-                        prompt: str_to_mut_i8("Here is a short greeting message in English: \""),
+                        prompt: str_to_mut_i8(&format!("{}", &entrytext)),
                         ..Default::default()
                     }
                 };
